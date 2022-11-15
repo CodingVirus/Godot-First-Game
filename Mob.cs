c44854771aed41c05b4762c543b0f614a4f67119
CodingVirus <glakffkdldk1@gmail.com>
2022-11-15 21:45:02 +0900

    ee

---
+++ Mob.cs
@@ -0,0 +1,14 @@
+using Godot;
+using System;
+
+public class Mob : RigidBody2D
+{
+    public override void _Ready()
+    {
+        var animSprite = GetNode<AnimatedSprite>("AnimatedSprite");
+        animSprite.Playing = true;
+        string[] mobTypes = animSprite.Frames.GetAnimationNames();
+        animSprite.Animation = mobTypes[GD.Randi() % mobTypes.Length];
+    }
+
+}
